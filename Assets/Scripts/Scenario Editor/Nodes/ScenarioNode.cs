15fafca3a07181434d4ef3bde16ff0f3eab20457
Caspar (Desktop) <caspar@glitchers.com>
2025-05-22 15:30:09 +0100

    feat(scenario editor): Matrices node that feeds entities to the Scenario Node / Testing Dynamic Ports and connection events

--- Assets/Scripts/Scenario Editor/Nodes/ScenarioNode.cs
+++ Assets/Scripts/Scenario Editor/Nodes/ScenarioNode.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 using XNode;
 
 /*public class Scenario
@@ -16,7 +18,7 @@ using XNode;
 	}
 }*/
 
-
+[NodeWidth(300)]
 public class ScenarioNode : Node
 {
     [SerializeField] protected string scenarioName;
@@ -30,19 +32,85 @@ public class ScenarioNode : Node
 
     [SerializeField] private int _seed;
 
+    private List<NodePort> _entityPorts;
+    public List<NodePort> EntityPorts => _entityPorts;
+
     public string Name => scenarioName;
 
     // Use this for initialization
     protected override void Init()
     {
         base.Init();
+        _entityPorts = new List<NodePort>();
+    }
 
+    private void OnValidate()
+    {
+        Debug.Log("VALIDATING SCENARIO");
     }
 
     // Return the correct value of an output port when requested
     public override object GetValue(NodePort port)
     {
+        foreach(NodePort entity in _entityPorts)
+        {
+            if (entity.fieldName == port.fieldName)
+            {
+                return entity.fieldName;
+            }
+        }
+
         return null;
+    }
+
+    public void ProcessMatrix()
+    {
+        //TODO(caspar): Tidy this whole thing up
+
+        if (GetInputPort("_matrix").IsConnected)
+        {
+            Matrix matrix = (Matrix)GetInputPort("_matrix").GetInputValue();
+            ClearEntityPorts();
+
+            //if (GetPort("EntityTest") == null)
+            {
+                foreach (string id in matrix.entityIDs)
+                {
+                    if (_entityPorts.FirstOrDefault(x => x.fieldName.Equals(id)) == null)
+                    {
+                        _entityPorts.Add(AddDynamicOutput(typeof(Entity), fieldName: id));
+                    }
+                }
+            }
+        }
+        else
+        {
+            //if (GetPort("EntityTest") != null)
+            {
+                ClearEntityPorts();
+            }
+        }
+    }
+
+    private void ClearEntityPorts()
+    {
+        foreach (NodePort port in _entityPorts)
+        {
+            RemoveDynamicPort(port);
+        }
+
+        _entityPorts.Clear();
+    }
+
+
+    public override void OnRemoveConnection(NodePort port)
+    {
+        base.OnRemoveConnection(port);
+
+        if (port == GetInputPort("_matrix"))
+        {
+            ClearEntityPorts();
+        }
     }
 
     /*#region Scenario
