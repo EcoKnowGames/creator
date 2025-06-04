8e2a3b139113efc30de1cddda8578d3ea68e5ebb
Caspar (Desktop) <caspar@glitchers.com>
2025-06-04 09:10:01 +0100

    feat(simulation): Minor cleanup after refactoring entity data

--- Assets/Scripts/Sandbox/Grid/Cell.cs
+++ Assets/Scripts/Sandbox/Grid/Cell.cs
@@ -6,20 +6,6 @@ using UnityEngine;
 
 namespace Glitchers.EcoKnow.Sandbox.Grid
 {
-    public class CellEntity
-    {
-        private string _id;
-        public string ID => _id;
-        private int _population;
-        public int Population { get => _population; set { _population = value; } }
-
-        public CellEntity(string id, int population)
-        {
-            _id = id;
-            _population = population;
-        }
-    }
-
     public class Cell : MonoBehaviour
     {
         [SerializeField] protected GameObject highlightObject;
