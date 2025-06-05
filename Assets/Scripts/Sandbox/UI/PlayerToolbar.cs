c077021a1fabd7be93cbf9b3fc68ef14803678d3
Caspar (Desktop) <caspar@glitchers.com>
2025-06-05 14:28:29 +0100

    feat(sandbox) ECO-14 Refactored HarvestModal into a generic ModifyCellModal for both Harvest and Introduce / Added Introduce quantity selection via the Modal

--- Assets/Scripts/Sandbox/UI/PlayerToolbar.cs
+++ Assets/Scripts/Sandbox/UI/PlayerToolbar.cs
@@ -6,12 +6,12 @@ namespace Glitchers.EcoKnow.Sandbox.UI
 {
     public class PlayerToolbar : MonoBehaviour
     {
-        public enum Action { NONE, INTRODUCE, HARVEST };
-        private Action _currentAction = Action.NONE;
+        public enum PlayerAction { NONE, INTRODUCE, HARVEST };
+        private PlayerAction _currentAction = PlayerAction.NONE;
 
         [Header("UI Elements")]
         [SerializeField] private TMP_Text _activeModeText;
-        [SerializeField] private HarvestModal _harvestModal;
+        [SerializeField] private ModifyCellModal _modifyCellModal;
 
         private Cell _selectedCell = null;
 
@@ -22,64 +22,44 @@ namespace Glitchers.EcoKnow.Sandbox.UI
                 SandboxManager.Instance.GridManager.gridEvents.OnCellClicked += OnCellClicked;
             }
 
-            SetCurrentAction(Action.NONE);
+            SetCurrentAction(PlayerAction.NONE);
 
-            _harvestModal?.HideModal();
+            _modifyCellModal?.HideModal();
         }
 
 
         public void OnCellClicked(Cell cell)
         {
-            if (_currentAction == Action.HARVEST)
+            if (_currentAction == PlayerAction.HARVEST)
             {
                 _selectedCell = cell;
                 ShowHarvestModal(_selectedCell.GetCellEntities());
-                return;
             }
-
-            //TODO(caspar) -> Do we need to sort this out???
-            //TODO(caspar) -> Sort this out? We probably want to check that the cell is valid before we try to do anything to it
-            //So within bounds, etc
-
-            bool success = false;
-
-            EntityManager entityManager = SandboxManager.Instance.EntityManager;
-            if (entityManager != null)
+            else if (_currentAction == PlayerAction.INTRODUCE)
             {
-                if (_currentAction == Action.HARVEST)
-                {
-                    success = SandboxManager.Instance.EntityManager.TryHarvestEntityFromCell(cell.Column, cell.Row, 0, 50);
-                }
-                else if (_currentAction == Action.INTRODUCE)
-                {
-                    success = SandboxManager.Instance.EntityManager.TryIntroduceEntityToCell(cell.Column, cell.Row, 0, 20);
-                }
-            }
-
-            if (success)
-            {
-                SetCurrentAction(Action.NONE);
+                _selectedCell = cell;
+                ShowIntroduceModal(_selectedCell.GetCellEntities());
             }
         }
 
         public void OnHarvestPressed()
         {
-            SetCurrentAction(Action.HARVEST);
+            SetCurrentAction(PlayerAction.HARVEST);
         }
 
         public void OnIntroducePressed()
         {
-            SetCurrentAction(Action.INTRODUCE);
+            SetCurrentAction(PlayerAction.INTRODUCE);
         }
 
         #region Actions
-        private void SetCurrentAction(Action action)
+        private void SetCurrentAction(PlayerAction action)
         {
             _currentAction = action;
 
             switch (_currentAction)
             {
-                case (Action.NONE):
+                case (PlayerAction.NONE):
                     {
                         _selectedCell = null;
                         break;
@@ -101,21 +81,44 @@ namespace Glitchers.EcoKnow.Sandbox.UI
         #region Harvest
         private void ShowHarvestModal(CellEntity[] cellEntities)
         {
-            _harvestModal?.ShowModal(cellEntities, OnHarvestConfirmed);
+            _modifyCellModal?.ShowModal("Harvest", cellEntities, OnHarvestConfirmed);
         }
 
         protected void OnHarvestConfirmed(int index, int amount)
         {
-            _harvestModal?.HideModal();
+            _modifyCellModal?.HideModal();
 
-            if (_selectedCell != null)
+            if ((_selectedCell != null) && (SandboxManager.Instance.EntityManager != null))
             {
                 bool success = false;
                 success = SandboxManager.Instance.EntityManager.TryHarvestEntityFromCell(_selectedCell.Column, _selectedCell.Row, index, amount);
 
                 if (success)
                 {
-                    SetCurrentAction(Action.NONE);
+                    SetCurrentAction(PlayerAction.NONE);
+                }
+            }
+        }
+        #endregion
+
+        #region Introduce
+        private void ShowIntroduceModal(CellEntity[] cellEntities)
+        {
+            _modifyCellModal?.ShowModal("Introduce", cellEntities, OnIntroduceConfirmed);
+        }
+
+        protected void OnIntroduceConfirmed(int index, int amount)
+        {
+            _modifyCellModal?.HideModal();
+
+            if ((_selectedCell != null) && (SandboxManager.Instance.EntityManager != null))
+            {
+                bool success = false;
+                success = SandboxManager.Instance.EntityManager.TryIntroduceEntityToCell(_selectedCell.Column, _selectedCell.Row, index, amount);
+
+                if (success)
+                {
+                    SetCurrentAction(PlayerAction.NONE);
                 }
             }
         }
