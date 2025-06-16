e8bdd42829ad0cd5cd80b286a2031a0196fcddd6
Caspar (Desktop) <caspar@glitchers.com>
2025-06-16 11:01:50 +0100

    feat(sandbox): Check Win Conditions at the end of each round / Player can Win/Lose the game / Restart game option on results modal

--- Assets/Scripts/Sandbox/UI/SandboxUI.cs
+++ Assets/Scripts/Sandbox/UI/SandboxUI.cs
@@ -8,14 +8,22 @@ namespace Glitchers.EcoKnow.Sandbox.UI
         [SerializeField] private RoundIndicator _roundIndicator;
         [SerializeField] private ObjectivePanel _objectivePanel;
         [SerializeField] private PlayerToolbar _playerToolbar;
+        [SerializeField] private ResultsModal _resultsModal;
 
         public void Init()
         {
             _objectivePanel?.Init();
             _playerToolbar?.Init();
+            _resultsModal?.HideModal();
         }
 
-        internal void OnNewRoundStarted(int currentRound, int maxRounds, int actions)
+
+        public void OnGameEnded(SandboxManager.Result result)
+        {
+            _resultsModal?.ShowModal(result.ToString());
+        }
+
+        public void OnNewRoundStarted(int currentRound, int maxRounds, int actions)
         {
             _roundIndicator?.UpdateRoundCounter(currentRound, maxRounds);
             _objectivePanel?.UpdatePopulations();
