67481cf57254659dfefd45efbfe214618ceb2647
Caspar (Desktop) <caspar@glitchers.com>
2025-06-17 17:21:02 +0100

    feat(scenario editor): ECO-28 Refactored Item/Currency Quantity nodes

---
+++ Assets/Scripts/Scenario Editor/Nodes/BaseQuantityNode.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public record Quantity
+(
+    string ID,
+    int Value
+);
+
+
+public abstract class BaseQuantityNode : Node
+{
+    [Output(ShowBackingValue.Always, ConnectionType.Override)] [SerializeField] private Quantity _quantity;
+
+    protected abstract string ID { get; }
+    [SerializeField] private int _value;
+    public int Value => _value;
+
+    // Use this for initialization
+    protected override void Init()
+    {
+        base.Init();
+
+    }
+
+    protected virtual Quantity GetQuantity()
+    {
+        return new Quantity(ID, Value);
+    }
+
+    // Return the correct value of an output port when requested
+    public override object GetValue(NodePort port)
+    {
+        if (port == GetOutputPort("_quantity"))
+        {
+            return GetQuantity();
+        }
+
+        return null;
+    }
+}
