85badfb6f675f57e312d80fb477a0017ee0177aa
Caspar (Desktop) <caspar@glitchers.com>
2025-06-19 15:34:37 +0100

    feat(sandbox): Added inventory panel / Added BGs to panels for clarity / Refactored inventory storage system / Display player's inventory in panel UI / Fix for inventory not updating before action complete event / Clear inventory on new game start

--- Assets/Scripts/Sandbox/UI/SandboxUI.cs
+++ Assets/Scripts/Sandbox/UI/SandboxUI.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Glitchers.EcoKnow.Sandbox.UI
 {
@@ -7,14 +8,21 @@ namespace Glitchers.EcoKnow.Sandbox.UI
     {
         [SerializeField] private RoundIndicator _roundIndicator;
         [SerializeField] private ObjectivePanel _objectivePanel;
+        [SerializeField] private InventoryPanel _inventoryPanel;
         [SerializeField] private PlayerToolbar _playerToolbar;
         [SerializeField] private ResultsModal _resultsModal;
 
         public void Init()
         {
             _objectivePanel?.Init();
+            _inventoryPanel?.Init();
             _playerToolbar?.Init();
             _resultsModal?.HideModal();
+
+            foreach(RectTransform child in this.GetComponentsInChildren<RectTransform>())
+            {
+                LayoutRebuilder.ForceRebuildLayoutImmediate(child);
+            }
         }
 
 
@@ -28,6 +36,7 @@ namespace Glitchers.EcoKnow.Sandbox.UI
             _roundIndicator?.UpdateRoundCounter(currentRound, maxRounds);
             _objectivePanel?.UpdatePopulations();
             _objectivePanel?.UpdateWinConditions();
+            _inventoryPanel?.RefreshInventory();
             _playerToolbar?.UpdateActionsRemaining(actions);
         }
     }
