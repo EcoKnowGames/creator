43e2d77081c832e8f17bbb37008c3828ca361cca
Caspar (Desktop) <caspar@glitchers.com>
2025-07-24 10:42:00 +0100

    feat(sandbox ui): Refactored the ModifyCell process into its own Manager and Modal components

---
+++ Assets/Scripts/Sandbox/UI/ModifyCellManager.cs
@@ -0,0 +1,234 @@
+using System;
+using System.Collections.Generic;
+using Glitchers.EcoKnow.Sandbox.Grid;
+using UnityEngine;
+
+namespace Glitchers.EcoKnow.Sandbox.UI
+{
+    public enum ModifyMode { NONE, INTRODUCE, HARVEST };
+
+    public class ModifyCellManager : MonoBehaviour
+    {
+        [SerializeField] private ModifyCellModal _modifyCellModal;
+        private SandboxUI _sandboxUI => this.GetComponentInParent<SandboxUI>();
+        private int _selectedEntityIndex => _sandboxUI != null ? _sandboxUI.SelectedEntityIndex : 0; //The parent UI should be the authority here
+
+
+
+        private ModifyMode _modifyMode = ModifyMode.NONE;
+        public ModifyMode CurrentMode => _modifyMode;
+        public bool IsModifying => _modifyMode != ModifyMode.NONE;
+
+
+        public Action<ModifyMode> onEnterModifyMode;
+        public Action onModifySuccess;
+        public Action onExitModifyMode;
+
+        private List<Cell> _selectedCells = new List<Cell>();
+
+        private const string LogChannel = "[ModifyCellManager]";
+
+        public void Init()
+        {
+            ExitModifyMode();
+
+            if (SandboxManager.Instance.GridManager != null)
+            {
+                SandboxManager.Instance.GridManager.OnCellClicked += OnCellClicked;
+            }
+        }
+
+        #region Callbacks
+        public void OnHarvestSelected()
+        {
+            if (SandboxManager.CanPerformAction())
+            {
+                EnterModifyMode(ModifyMode.HARVEST);
+            }
+            else
+            {
+                Debug.LogError($"{LogChannel} Cannot select action, no action points remaining!");
+            }
+        }
+
+        public void OnIntroduceSelected()
+        {
+            if (SandboxManager.CanPerformAction())
+            {
+                EnterModifyMode(ModifyMode.INTRODUCE);
+            }
+            else
+            {
+                Debug.LogError($"{LogChannel} Cannot select action, no action points remaining!");
+            }
+        }
+
+        public void OnInputModified()
+        {
+            _modifyCellModal?.UpdateModal(_selectedEntityIndex, _selectedCells);
+        }
+
+        public void OnCellClicked(Cell cell)
+        {
+            if (IsModifying)
+            {
+                if (_selectedCells.Contains(cell))
+                {
+                    _selectedCells.Remove(cell);
+                    cell.ShowSelected(false);
+                }
+                else
+                {
+                    _selectedCells.Add(cell);
+                    cell.ShowSelected(true);
+                }
+
+                _modifyCellModal?.UpdateModal(_selectedEntityIndex, _selectedCells);
+            }
+        }
+
+        private void ClearSelectedCells()
+        {
+            foreach (Cell cell in _selectedCells)
+            {
+                cell.ShowSelected(false);
+            }
+
+            _selectedCells.Clear();
+        }
+        #endregion
+
+        private void EnterModifyMode(ModifyMode mode)
+        {
+            _modifyMode = mode;
+            ClearSelectedCells();
+
+            switch (_modifyMode)
+            {
+                case (ModifyMode.HARVEST):
+                    {
+                        ShowHarvestModal(_selectedEntityIndex);
+                        break;
+                    }
+
+                case (ModifyMode.INTRODUCE):
+                    {
+                        ShowIntroduceModal(_selectedEntityIndex);
+                        break;
+                    }
+                case (ModifyMode.NONE):
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+
+            onEnterModifyMode?.Invoke(_modifyMode);
+        }
+
+        public void ExitModifyMode()
+        {
+            _modifyMode = ModifyMode.NONE;
+            _modifyCellModal?.HideModal();
+            ClearSelectedCells();
+
+            onExitModifyMode?.Invoke();
+        }
+
+        #region Events
+        private void OnModifySuccess(ModifyMode modifyMode)
+        {
+            ExitModifyMode();
+            int actionsRemaining = SandboxManager.SpendActionPoint();
+
+            if (modifyMode == ModifyMode.HARVEST)
+            {
+                Data.DataManager.Instance.RecordEvent(Data.EventType.HARVEST);
+            }
+            else if (modifyMode == ModifyMode.INTRODUCE)
+            {
+                Data.DataManager.Instance.RecordEvent(Data.EventType.INTRODUCE);
+            }
+
+            onModifySuccess?.Invoke();
+        }
+
+        private void OnModifyCancelled()
+        {
+            ExitModifyMode();
+        }
+        #endregion
+
+        #region Harvest
+        private void ShowHarvestModal(int entityIndex)
+        {
+            _modifyCellModal?.ShowModal(ModifyMode.HARVEST, entityIndex, OnInputModified, OnHarvestConfirmed, OnModifyCancelled);
+        }
+
+        protected void OnHarvestConfirmed(ModifyCellModal.UnitMode unitMode, float amount)
+        {
+            _modifyCellModal?.HideModal();
+
+            int successCount = 0;
+            if ((SandboxManager.Instance.EntityManager != null) && (_selectedCells.Count > 0))
+            {
+                foreach (Cell cell in _selectedCells)
+                {
+                    int cellPopulation = SandboxManager.Instance.EntityManager.GetPopulationInCell(cell.Column, cell.Row, _selectedEntityIndex);
+                    int actualAmount = unitMode == ModifyCellModal.UnitMode.DISCRETE ? Mathf.FloorToInt(amount) : Mathf.FloorToInt(cellPopulation * (amount / 100f));
+                    if (SandboxManager.Instance.EntityManager.TryHarvestEntityFromCell(cell.Column, cell.Row, _selectedEntityIndex, actualAmount))
+                    {
+                        successCount += 1;
+                    }
+                }
+            }
+
+            if (successCount > 0)
+            {
+                OnModifySuccess(ModifyMode.HARVEST);
+            }
+            else
+            {
+                OnModifyCancelled();
+            }
+        }
+        #endregion
+
+        #region Introduce
+        private void ShowIntroduceModal(int entityIndex)
+        {
+            _modifyCellModal?.ShowModal(ModifyMode.INTRODUCE, entityIndex, OnInputModified, OnIntroduceConfirmed, OnModifyCancelled);
+        }
+
+        protected void OnIntroduceConfirmed(ModifyCellModal.UnitMode unitMode, float amount)
+        {
+            _modifyCellModal?.HideModal();
+
+            int successCount = 0;
+            if ((SandboxManager.Instance.EntityManager != null) && (_selectedCells.Count > 0))
+            {
+                foreach (Cell cell in _selectedCells)
+                {
+                    int actualAmount = Mathf.FloorToInt(amount);
+                    if (SandboxManager.Instance.EntityManager.TryIntroduceEntityToCell(cell.Column, cell.Row, _selectedEntityIndex, actualAmount))
+                    {
+                        successCount += 1;
+                    }
+                }
+            }
+
+            if (successCount > 0)
+            {
+                OnModifySuccess(ModifyMode.INTRODUCE);
+            }
+            else
+            {
+                OnModifyCancelled();
+            }
+        }
+        #endregion
+    }
+}
--- Assets/Scripts/Sandbox/UI/SandboxUI.cs
+++ Assets/Scripts/Sandbox/UI/SandboxUI.cs
@@ -6,8 +6,7 @@ using UnityEngine.UI;
 
 namespace Glitchers.EcoKnow.Sandbox.UI
 {
-    //TODO(caspar): Maybe "Modify Mode" needs to live in a Modify handler script rather than at the top level of the SandboxUI
-    public enum ModifyMode { NONE, INTRODUCE, HARVEST };
+
 
     public class SandboxUI : MonoBehaviour
     {
@@ -16,11 +15,11 @@ namespace Glitchers.EcoKnow.Sandbox.UI
         [SerializeField] private ObjectivePanel _objectivePanel;
         [SerializeField] private InventoryPanel _inventoryPanel;
         [SerializeField] private PlayerToolbar _playerToolbar;
-        [SerializeField] private ModifyCellModal _modifyCellModal;
+        [SerializeField] private ModifyCellManager _modifyCellManager;
         [SerializeField] private ResultsModal _resultsModal;
 
         private int _selectedEntityIndex = 0;
-        private ModifyMode _currentMode = ModifyMode.NONE;
+        public int SelectedEntityIndex => _selectedEntityIndex;
 
         private const string LogChannel = "[SandboxUI]";
 
@@ -32,13 +31,14 @@ namespace Glitchers.EcoKnow.Sandbox.UI
             _objectivePanel?.Init();
             _inventoryPanel?.Init();
             _playerToolbar?.Init();
-            _modifyCellModal?.HideModal();
+            _modifyCellManager?.Init();
             _resultsModal?.HideModal();
 
             //Subscribe to UI events
             _entityPanel.onEntitySelected += OnEntitySelected;
-
-            SetCurrentMode(ModifyMode.NONE);
+            _modifyCellManager.onEnterModifyMode += OnEnterModifyMode;
+            _modifyCellManager.onExitModifyMode += OnExitModifyMode;
+            _modifyCellManager.onModifySuccess += OnModifySuccess;
 
             //Force rebuild
             foreach (RectTransform child in this.GetComponentsInChildren<RectTransform>())
@@ -53,7 +53,7 @@ namespace Glitchers.EcoKnow.Sandbox.UI
             //Forces reset
             if (entityIndex != _selectedEntityIndex)
             {
-                _modifyCellModal?.HideModal();
+                _modifyCellManager?.ExitModifyMode();
             }
 
             //Clamp just in case?
@@ -79,157 +79,28 @@ namespace Glitchers.EcoKnow.Sandbox.UI
             _objectivePanel?.UpdatePopulations();
             _objectivePanel?.UpdateWinConditions();
             _inventoryPanel?.RefreshInventory();
+            _modifyCellManager?.ExitModifyMode();
             _playerToolbar?.UpdateActionsRemaining(actions);
         }
         #endregion
 
-        #region Harvest and Introduce
-        public void OnHarvestSelected()
+        #region Modify Mode
+        
+
+        private void OnEnterModifyMode(ModifyMode mode)
         {
-            if (SandboxManager.CanPerformAction())
-            {
-                SetCurrentMode(ModifyMode.HARVEST);
-            }
-            else
-            {
-                Debug.LogError($"{LogChannel} Cannot select action, no action points remaining!");
-            }
+            _playerToolbar?.SetModifyModeText(mode);
         }
 
-        public void OnIntroduceSelected()
+        private void OnModifySuccess()
         {
-            if (SandboxManager.CanPerformAction())
-            {
-                SetCurrentMode(ModifyMode.INTRODUCE);
-            }
-            else
-            {
-                Debug.LogError($"{LogChannel} Cannot select action, no action points remaining!");
-            }
-        }
-
-        private void SetCurrentMode(ModifyMode mode)
-        {
-            _currentMode = mode;
-
-            switch (_currentMode)
-            {
-                case (ModifyMode.HARVEST):
-                    {
-                        ShowHarvestModal(_selectedEntityIndex);
-                        break;
-                    }
-
-                case (ModifyMode.INTRODUCE):
-                    {
-                        ShowIntroduceModal(_selectedEntityIndex);
-                        break;
-                    }
-                case (ModifyMode.NONE):
-                    {
-                        break;
-                    }
-                default:
-                    {
-                        break;
-                    }
-            }
-
-            //TODO(caspar): We won't need this later, but useful for now
-            _playerToolbar?.SetModifyModeText(_currentMode);
-        }
-
-        private void OnModifySuccess(ModifyMode modifyMode)
-        {
-            SetCurrentMode(ModifyMode.NONE);
-            int actionsRemaining = SandboxManager.SpendActionPoint();
-
-            if (modifyMode == ModifyMode.HARVEST)
-            {
-                Data.DataManager.Instance.RecordEvent(Data.EventType.HARVEST);
-            }
-            else if (modifyMode == ModifyMode.INTRODUCE)
-            {
-                Data.DataManager.Instance.RecordEvent(Data.EventType.INTRODUCE);
-            }
-
-            //Update some UI elements
+            int actionsRemaining = SandboxManager.GetAvailableActionPoints();
             _playerToolbar?.UpdateActionsRemaining(actionsRemaining);
         }
 
-        private void OnModifyCancelled()
+        private void OnExitModifyMode()
         {
-            SetCurrentMode(ModifyMode.NONE);
             _playerToolbar?.HideToolbar();
-        }
-        #endregion
-
-        #region Harvest
-        private void ShowHarvestModal(int entityIndex)
-        {
-            _modifyCellModal?.ShowModal(ModifyMode.HARVEST, entityIndex, OnHarvestConfirmed, OnModifyCancelled);
-        }
-
-        protected void OnHarvestConfirmed(int index, List<Cell> selectedCells, ModifyCellModal.UnitMode unitMode, float amount)
-        {
-            _modifyCellModal?.HideModal();
-
-            int successCount = 0;
-            if ((SandboxManager.Instance.EntityManager != null) && (selectedCells.Count > 0))
-            {
-                foreach(Cell cell in selectedCells)
-                {
-                    int currentPopulation = cell.GetCellEntities()[index].Population;
-                    int actualAmount = unitMode == ModifyCellModal.UnitMode.DISCRETE ? Mathf.FloorToInt(amount) : Mathf.FloorToInt(currentPopulation * (amount / 100f));
-                    if (SandboxManager.Instance.EntityManager.TryHarvestEntityFromCell(cell.Column, cell.Row, index, actualAmount))
-                    {
-                        successCount += 1;
-                    }
-                }
-            }
-
-            if (successCount > 0)
-            {
-                OnModifySuccess(ModifyMode.HARVEST);
-            }
-            else
-            {
-                OnModifyCancelled();
-            }
-        }
-        #endregion
-
-        #region Introduce
-        private void ShowIntroduceModal(int entityIndex)
-        {
-            _modifyCellModal?.ShowModal(ModifyMode.INTRODUCE, entityIndex, OnIntroduceConfirmed, OnModifyCancelled);
-        }
-
-        protected void OnIntroduceConfirmed(int index, List<Cell> selectedCells, ModifyCellModal.UnitMode unitMode, float amount)
-        {
-            _modifyCellModal?.HideModal();
-
-            int successCount = 0;
-            if ((SandboxManager.Instance.EntityManager != null) && (selectedCells.Count > 0))
-            {
-                foreach (Cell cell in selectedCells)
-                {
-                    int actualAmount = Mathf.FloorToInt(amount);
-                    if (SandboxManager.Instance.EntityManager.TryIntroduceEntityToCell(cell.Column, cell.Row, index, actualAmount))
-                    {
-                        successCount += 1;
-                    }
-                }
-            }
-
-            if (successCount > 0)
-            {
-                OnModifySuccess(ModifyMode.INTRODUCE);
-            }
-            else
-            {
-                OnModifyCancelled();
-            }
         }
         #endregion
     }
