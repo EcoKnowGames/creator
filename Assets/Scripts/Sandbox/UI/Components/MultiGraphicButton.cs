3dacf15f9bb6a77d6fddeb6f403204f6c7b61cee
Caspar (Desktop) <caspar@glitchers.com>
2025-07-30 17:35:56 +0100

    feat(sandbox ui): Fix for MultiGraphicButton not applying colours correctly on the primary graphic

--- Assets/Scripts/Sandbox/UI/Components/MultiGraphicButton.cs
+++ Assets/Scripts/Sandbox/UI/Components/MultiGraphicButton.cs
@@ -3,20 +3,63 @@ using UnityEngine.UI;
 
 public class MultiGraphicButton : Button
 {
-
     private GraphicColourTint[] _graphics => this.GetComponentsInChildren<GraphicColourTint>();
 
     protected override void DoStateTransition(SelectionState state, bool instant)
     {
-        base.DoStateTransition(state, instant);
-        SetStateForAllGraphics(state);
+        switch (this.transition)
+        {
+            case Transition.ColorTint:
+                SetStateForAllGraphics(state);
+                break;
+            case Transition.None:
+                break;
+            default:
+                throw new System.NotSupportedException(string.Format("MultiGraphicButton does not support this transition type - {0}", this.transition));
+        }
     }
 
     private void SetStateForAllGraphics(SelectionState state)
     {
+        //Set all our tints
         foreach(GraphicColourTint graphic in _graphics)
         {
             graphic.SetState((int)state);
+        }
+
+        //Override standard ColorTint behaviour with our own
+        Graphic localGraphic = this.GetComponent<Graphic>();
+        if (localGraphic != null)
+        {
+            switch (state)
+            {
+                case (SelectionState.Highlighted):
+                    {
+                        localGraphic.color = colors.highlightedColor;
+                        break;
+                    }
+                case (SelectionState.Pressed):
+                    {
+                        localGraphic.color = colors.pressedColor;
+                        break;
+                    }
+                case (SelectionState.Selected):
+                    {
+                        localGraphic.color = colors.selectedColor;
+                        break;
+                    }
+                case (SelectionState.Disabled):
+                    {
+                        localGraphic.color = colors.disabledColor;
+                        break;
+                    }
+                case (SelectionState.Normal):
+                default:
+                    {
+                        localGraphic.color = colors.normalColor;
+                        break;
+                    }
+            }
         }
     }
 }
