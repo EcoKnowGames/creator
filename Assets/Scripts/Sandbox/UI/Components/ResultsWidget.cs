c16bc53d699c4468e0740d81877e7f01f774e149
Caspar (Desktop) <caspar@glitchers.com>
2025-08-05 16:47:00 +0100

    feat(win conditions): Update Objectives UI to display the correct results for each round / Fade out or show checkmark based on completion/failure state / Show future state correctly

--- Assets/Scripts/Sandbox/UI/Components/ResultsWidget.cs
+++ Assets/Scripts/Sandbox/UI/Components/ResultsWidget.cs
@@ -1,16 +1,84 @@
 using UnityEngine;
 
-public class ResultsWidget : MonoBehaviour
+namespace Glitchers.EcoKnow.Sandbox.UI
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class ResultsWidget : MonoBehaviour
     {
-        
-    }
+        //This matches the states in WinCondition
+        //but includes extra states like FUTURE that are specific to the tracker
+        public enum Result
+        {
+            NOT_STARTED = 0,
+            IN_RANGE = 1,
+            STREAK = 2,
+            GRACE = 3,
+            FAILED = 4,
+            FUTURE = 5
+        };
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Header("States")]
+        [SerializeField] private GameObject _notStartedState;
+        [SerializeField] private GameObject _inRangeState;
+        [SerializeField] private GameObject _streakState;
+        [SerializeField] private GameObject _graceState;
+        [SerializeField] private GameObject _failedState;
+        [SerializeField] private GameObject _futureState;
+
+        //TODO(caspar): Active range
+
+        private Result _currentState;
+
+        public void SetState(int state)
+        {
+            _currentState = (Result)state;
+            HideAllStates();
+
+            switch (_currentState)
+            {
+                case (Result.FAILED):
+                    {
+                        _failedState?.SetActive(true);
+                        break;
+                    }
+                case (Result.GRACE):
+                    {
+                        _graceState?.SetActive(true);
+                        break;
+                    }
+                case (Result.STREAK):
+                    {
+                        _streakState?.SetActive(true);
+                        break;
+                    }
+                case (Result.IN_RANGE):
+                    {
+                        _inRangeState?.SetActive(true);
+                        break;
+                    }
+                case (Result.FUTURE):
+                    {
+                        _futureState?.SetActive(true);
+                        break;
+                    }
+                case (Result.NOT_STARTED):
+                default:
+                    {
+                        _notStartedState?.SetActive(true);
+                        break;
+                    }
+
+            }
+
+        }
+
+        private void HideAllStates()
+        {
+            _notStartedState?.SetActive(false);
+            _inRangeState?.SetActive(false);
+            _streakState?.SetActive(false);
+            _graceState?.SetActive(false);
+            _failedState?.SetActive(false);
+            _futureState?.SetActive(false);
+        }
     }
 }
