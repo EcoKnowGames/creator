78c79b695f4834781d2f41c3da268bf8c66db163
Caspar (Desktop) <caspar@glitchers.com>
2025-08-05 18:25:09 +0100

    feat(win conditions): Update active results widget with current population / Resize results widget to fit text

--- Assets/Scripts/Sandbox/UI/Components/ResultsWidget.cs
+++ Assets/Scripts/Sandbox/UI/Components/ResultsWidget.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro;
 
 namespace Glitchers.EcoKnow.Sandbox.UI
 {
@@ -6,14 +7,15 @@ namespace Glitchers.EcoKnow.Sandbox.UI
     {
         //This matches the states in WinCondition
         //but includes extra states like FUTURE that are specific to the tracker
-        public enum Result
+        public enum State
         {
             NOT_STARTED = 0,
             IN_RANGE = 1,
             STREAK = 2,
             GRACE = 3,
             FAILED = 4,
-            FUTURE = 5
+            ACTIVE = 5,
+            FUTURE = 6
         };
 
         [Header("States")]
@@ -22,45 +24,54 @@ namespace Glitchers.EcoKnow.Sandbox.UI
         [SerializeField] private GameObject _streakState;
         [SerializeField] private GameObject _graceState;
         [SerializeField] private GameObject _failedState;
+        [SerializeField] private GameObject _activeState;
         [SerializeField] private GameObject _futureState;
 
         //TODO(caspar): Active range
+        [Header("Active")]
+        [SerializeField] private TMP_Text _activeText;
 
-        private Result _currentState;
+        private State _currentState;
+        public bool IsActive => _currentState == State.ACTIVE;
 
         public void SetState(int state)
         {
-            _currentState = (Result)state;
+            _currentState = (State)state;
             HideAllStates();
 
             switch (_currentState)
             {
-                case (Result.FAILED):
+                case (State.FAILED):
                     {
                         _failedState?.SetActive(true);
                         break;
                     }
-                case (Result.GRACE):
+                case (State.GRACE):
                     {
                         _graceState?.SetActive(true);
                         break;
                     }
-                case (Result.STREAK):
+                case (State.STREAK):
                     {
                         _streakState?.SetActive(true);
                         break;
                     }
-                case (Result.IN_RANGE):
+                case (State.IN_RANGE):
                     {
                         _inRangeState?.SetActive(true);
                         break;
                     }
-                case (Result.FUTURE):
+                case (State.ACTIVE):
+                    {
+                        _activeState?.SetActive(true);
+                        break;
+                    }
+                case (State.FUTURE):
                     {
                         _futureState?.SetActive(true);
                         break;
                     }
-                case (Result.NOT_STARTED):
+                case (State.NOT_STARTED):
                 default:
                     {
                         _notStartedState?.SetActive(true);
@@ -68,7 +79,6 @@ namespace Glitchers.EcoKnow.Sandbox.UI
                     }
 
             }
-
         }
 
         private void HideAllStates()
@@ -78,7 +88,32 @@ namespace Glitchers.EcoKnow.Sandbox.UI
             _streakState?.SetActive(false);
             _graceState?.SetActive(false);
             _failedState?.SetActive(false);
+            _activeState?.SetActive(false);
             _futureState?.SetActive(false);
+        }
+
+        public void SetActiveQuantity(int quantity)
+        {
+            if (_activeText != null)
+            {
+                _activeText.text = FormatQuantity(quantity);
+            }
+        }
+
+        private string FormatQuantity(int quantity)
+        {
+            if (quantity >= 1000000)
+            {
+                float roundedQuantity = Mathf.Floor(((float)quantity / 100000f) * 10f) / 10f;
+                return roundedQuantity.ToString("0.#") + "M";
+            }
+            else if (quantity >= 1000)
+            {
+                float roundedQuantity = Mathf.Floor(((float)quantity / 1000f) * 10f) / 10f;
+                return roundedQuantity.ToString("0.#") + "k";
+            }
+
+            return quantity.ToString();
         }
     }
 }
