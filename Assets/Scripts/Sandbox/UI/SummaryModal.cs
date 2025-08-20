c4b6232763c4fdd326a85c02381c3d65f2903bd6
Caspar (Desktop) <caspar@glitchers.com>
2025-08-20 18:12:50 +0100

    feat(summary screen): Refactored ResultsTracker into its own script / Set Entity and Win Condition information on the Summary Screen / Init correct number of widgets for win conditions on Summary Screen

--- Assets/Scripts/Sandbox/UI/SummaryModal.cs
+++ Assets/Scripts/Sandbox/UI/SummaryModal.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
 using TMPro;
 using Glitchers.EcoKnow.Sandbox;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Glitchers.EcoKnow.Sandbox.UI
 {
-
     public class SummaryModal : MonoBehaviour
     {
+        [Header("Objective Results")]
+        [SerializeField] private ObjectiveResultWidget _objectiveWidgetPrefab;
+        [SerializeField] private Transform _objectiveContainer;
+
+        private const string LogChannel = "[SummaryModal]";
+
         public void ShowModal()
         {
+            SetupResults();
             this.gameObject.SetActive(true);
         }
 
@@ -35,5 +43,35 @@ namespace Glitchers.EcoKnow.Sandbox.UI
             //TODO(caspar): Do we want to allow data export on WebGL? This will likely require some js
 #endif
         }
+
+        #region Win Conditions
+        private void SetupResults()
+        {
+            if ((_objectiveWidgetPrefab == null) || (_objectiveContainer == null))
+            {
+                Debug.LogError($"{LogChannel} Failed setup, Widget prefab or Container is null!");
+                return;
+            }
+
+            //Remove old objectives
+            foreach (Transform child in _objectiveContainer)
+            {
+                Destroy(child.gameObject);
+            }
+
+            //Instantiate new ones
+            List<WinCondition> winConditions = SandboxManager.Instance.WinConditions.OrderByDescending(x => x.Completed).ToList();
+
+            foreach (WinCondition condition in winConditions)
+            {
+                ObjectiveResultWidget widget = Instantiate(_objectiveWidgetPrefab, _objectiveContainer);
+                if (widget != null)
+                {
+                    widget.Init(condition);
+                }
+            }
+        }
+
+        #endregion
     }
 }
