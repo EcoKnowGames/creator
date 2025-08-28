36fd5df7f94654d0b73d385c03e6d2d4c18e41ed
Caspar (Desktop) <caspar@glitchers.com>
2025-08-28 15:53:08 +0100

    feat(multiplayer): Initial multiplayer functionality / Increment player index on action completed / Display player index on border UI / Auto-advance to next round in multiplayer / Record player index in data

---
+++ Assets/Scripts/Sandbox/UI/Components/MultiplayerBorder.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using TMPro;
+
+namespace Glitchers.EcoKnow.Sandbox.UI
+{
+    public class MultiplayerBorder : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup _canvasGroup => this.GetComponent<CanvasGroup>();
+        [SerializeField] private TMP_Text _indicatorText;
+
+        public void SetPlayer(int index)
+        {
+            if (_indicatorText != null)
+            {
+                _indicatorText.text = string.Format($"Player {index + 1}'s Turn"); //Account for 0
+            }
+        }
+
+        public void SetBorderVisible(bool visible)
+        {
+            if (_canvasGroup != null)
+            {
+                _canvasGroup.alpha = visible ? 1f : 0f;
+            }
+        }
+    }
+}
