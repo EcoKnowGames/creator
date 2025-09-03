ace896ff74c2057d2925b9ca17331741bf1b662d
Caspar (Desktop) <caspar@glitchers.com>
2025-09-03 17:57:00 +0100

    feat(main menu): Player Setup screen now controls max players and provides visual feedback on user selection / Load scenario from player setup screen / Fixed background colour issue in-game

--- Assets/Scripts/Sandbox/Menus/MenuScreen_Setup.cs
+++ Assets/Scripts/Sandbox/Menus/MenuScreen_Setup.cs
@@ -7,20 +7,17 @@ namespace Glitchers.EcoKnow.Sandbox.Menus
         private MainMenuController.Screen _entryScreen = MainMenuController.Screen.SELECT_SCENARIO;
 
         [Header("Buttons")]
-        [SerializeField] private GameObject _playerButton_One;
-        [SerializeField] private GameObject _playerButton_Two;
-        [SerializeField] private GameObject _playerButton_Three;
-        [SerializeField] private GameObject _playerButton_Four;
+        [SerializeField] private Button_PlayerSelect _playerButton_One;
+        [SerializeField] private Button_PlayerSelect _playerButton_Two;
+        [SerializeField] private Button_PlayerSelect _playerButton_Three;
+        [SerializeField] private Button_PlayerSelect _playerButton_Four;
 
         public override void Show()
         {
             base.Show();
 
-            ScenarioConfig config = ScenarioLoader.Instance.LoadedConfig;
-            if (config != null)
-            {
-                //int ac
-            }
+            MultiplayerManager.Instance?.SetMaxPlayers(1); //Singleplayer is default
+            RefreshButtons();
         }
 
         public void SetEntryScreen(MainMenuController.Screen entryScreen)
@@ -31,6 +28,54 @@ namespace Glitchers.EcoKnow.Sandbox.Menus
         public void OnReturnPressed()
         {
             MainMenuController?.SetScreen(_entryScreen);
+        }
+
+        public void OnStartGamePressed()
+        {
+            MainMenuController.RequestLoadSandboxScene(() => ScenarioLoader.Instance.RequestStartLoadedConfig());
+        }
+
+        public void SetMaxPlayers(int maxPlayers)
+        {
+            MultiplayerManager.Instance?.SetMaxPlayers(maxPlayers);
+            RefreshButtons();
+        }
+
+        private void RefreshButtons()
+        {
+            SetSelectedPlayerButtons();
+            SetValidPlayerButtons();
+        }
+
+        private void SetSelectedPlayerButtons()
+        {
+            //Setup Selected
+            int maxPlayers = MultiplayerManager.Instance.MaxPlayers;
+            _playerButton_One?.SetSelected(maxPlayers == 1);
+            _playerButton_Two?.SetSelected(maxPlayers == 2);
+            _playerButton_Three?.SetSelected(maxPlayers == 3);
+            _playerButton_Four?.SetSelected(maxPlayers == 4);
+
+            _playerButton_One?.SetPlayerFieldInteractable(true);
+            _playerButton_Two?.SetPlayerFieldInteractable(2 <= maxPlayers);
+            _playerButton_Three?.SetPlayerFieldInteractable(3 <= maxPlayers);
+            _playerButton_Four?.SetPlayerFieldInteractable(4 <= maxPlayers);
+        }
+
+        private void SetValidPlayerButtons()
+        {
+            //Setup Interactable
+            int maxActions = 1;
+            ScenarioConfig config = ScenarioLoader.Instance.LoadedConfig;
+            if (config != null)
+            {
+                maxActions = config.Scenario.ActionsPerRound;
+            }
+
+            _playerButton_One?.SetInteractable(true);
+            _playerButton_Two?.SetInteractable(maxActions % 2 == 0);
+            _playerButton_Three?.SetInteractable(maxActions % 3 == 0);
+            _playerButton_Four?.SetInteractable(maxActions % 4 == 0);
         }
     }
 }
