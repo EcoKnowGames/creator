9fcbb4faa75d85b02901e88215f68352da9f0668
Caspar (Desktop) <caspar@glitchers.com>
2025-09-03 15:56:23 +0100

    feat(main menu): Some initial Setup Scenario menu UI setup

--- Assets/Scripts/Sandbox/Menus/MenuScreen_Setup.cs
+++ Assets/Scripts/Sandbox/Menus/MenuScreen_Setup.cs
@@ -6,6 +6,23 @@ namespace Glitchers.EcoKnow.Sandbox.Menus
     {
         private MainMenuController.Screen _entryScreen = MainMenuController.Screen.SELECT_SCENARIO;
 
+        [Header("Buttons")]
+        [SerializeField] private GameObject _playerButton_One;
+        [SerializeField] private GameObject _playerButton_Two;
+        [SerializeField] private GameObject _playerButton_Three;
+        [SerializeField] private GameObject _playerButton_Four;
+
+        public override void Show()
+        {
+            base.Show();
+
+            ScenarioConfig config = ScenarioLoader.Instance.LoadedConfig;
+            if (config != null)
+            {
+                //int ac
+            }
+        }
+
         public void SetEntryScreen(MainMenuController.Screen entryScreen)
         {
             _entryScreen = entryScreen;
